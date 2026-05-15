7fab94b701f78758509fa2df7a3bbf08348b33bd
sainianshul <anshulsainionline@gmail.com>
2026-05-15 23:25:07 +0530

    Correct Sidebar Code

--- resources/views/admin/layouts/partials/_sidebar.blade.php
+++ resources/views/admin/layouts/partials/_sidebar.blade.php
@@ -1,7 +1,5 @@
 <!--begin::Sidebar-->
-<div id="kt_app_sidebar" class="app-sidebar flex-column" data-kt-drawer="true" data-kt-drawer-name="app-sidebar"
-    data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="250px"
-    data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_app_sidebar_mobile_toggle">
+<div id="kt_app_sidebar" class="app-sidebar flex-column">
 
     <!--begin::Logo-->
     <div class="app-sidebar-logo flex-shrink-0 d-none d-md-flex align-items-center px-8" id="kt_app_sidebar_logo">
@@ -15,13 +13,10 @@
 
     <!--begin::Sidebar menu-->
     <div class="app-sidebar-menu overflow-hidden flex-column-fluid">
-        <div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5 mx-3"
-            data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto"
-            data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer"
-            data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px">
+        <div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper my-5 mx-3"
+            style="overflow-y:auto; flex:1 1 auto;">
 
-            <div class="menu menu-column menu-rounded menu-sub-indention fw-semibold px-1" id="#kt_app_sidebar_menu"
-                data-kt-menu="true" data-kt-menu-expand="false">
+            <div class="menu menu-column menu-rounded menu-sub-indention fw-semibold px-1" id="kt_app_sidebar_menu">
 
                 {{-- ===================== --}}
                 {{-- DASHBOARD --}}
